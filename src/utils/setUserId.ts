98fdb4f62c12edebf92cbaedddef2e3a4bbdb652
ahmad-mahmood1 <ahmad.mahmood.a90@gmail.com>
2023-01-29 02:15:12 +0500

    sameSite fix

--- src/utils/setUserId.ts
+++ src/utils/setUserId.ts
@@ -1,12 +1,12 @@
-import { Response } from "express";
+import { CookieOptions, Response } from "express";
 import { __prod__ } from "../constants";
 
 export const setUserCookie = (res: Response, userId: number) => {
-  const options = {
+  const options: CookieOptions = {
     signed: true,
     maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
     httpOnly: true,
-    samesite: __prod__ ? "none" : "lax",
+    sameSite: __prod__ ? "none" : "lax",
     secure: __prod__ ? true : false,
   };
 
